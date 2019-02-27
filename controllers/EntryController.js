9e190bca7bb29b93fad48104cc65f8545af77182
tahtaciburak <tahtaciburak@gmail.com>
2019-02-28 00:03:24 +0300

    Added expiration feature

--- controllers/EntryController.js
+++ controllers/EntryController.js
@@ -8,7 +8,7 @@ exports.getDefaultPage = function(req, res) {
 };
 
 exports.getEntryDetailPage = function(req, res, next) {
-    db.query("SELECT * FROM paste WHERE id=?",[parseInt(utils.url2id(req.params["key"])-999999)],function(err,result){
+    db.query("SELECT * FROM paste WHERE id=? AND expiration > NOW()",[parseInt(utils.url2id(req.params["key"])-999999)],function(err,result){
         if (err) return err;
         if (result[0] !== undefined) {
             if (result[0].secret){
@@ -16,12 +16,26 @@ exports.getEntryDetailPage = function(req, res, next) {
             }else{
                 res.render('index', { status:"details" , isEncrypted: false, content: result[0].text})    
             }
+        }else{
+            res.redirect("/");
         }
     });
 };
 
 exports.saveEntry = function(req, res, next) {    
-    db.query("INSERT INTO paste(text,secret,expiration,created_at) values(? , ? , DATE_ADD(NOW(), INTERVAL 1 YEAR),NOW());",[req.body["text"],req.body["secret"]],function(err,result){
+    console.log(req.body["expiration"])
+    let interval = "1 YEAR";
+    if (req.body["expiration"] === "month"){
+        interval = "1 MONTH";
+    } else if (req.body["expiration"] === "day"){
+        interval = "1 DAY"
+    } else if (req.body["expiration"] === "hour"){
+        interval = "1 HOUR"
+    } else if (req.body["expiration"] === "minute"){
+        interval = "5 MINUTES"
+    }
+
+    db.query("INSERT INTO paste(text,secret,expiration,created_at) values(? , ? , DATE_ADD(NOW(), INTERVAL "+interval+"),NOW());",[req.body["text"],req.body["secret"]],function(err,result){
         if (err) throw err; 
         res.send( { "url": req.headers.host+"/p/"+utils.id2url(result.insertId+999999) } );
     });    
