26a67965918a1b857895ddc936e9a8557b6866dc
tahtaciburak <tahtaciburak@gmail.com>
2019-03-02 18:22:53 +0300

    secret keys more secure

--- controllers/EntryController.js
+++ controllers/EntryController.js
@@ -2,6 +2,8 @@
 var entry = require("../models/Entry");
 var db = require("../lib/db");
 var utils = require("../lib/utils");
+var cryptoUtils = require("../lib/cryptoUtils");
+var crypto = require('crypto');
 
 exports.getDefaultPage = function(req, res) {
     res.render('index', { status:"new", isEncrypted: false, content: "Write something here..." });
@@ -23,7 +25,7 @@ exports.getEntryDetailPage = function(req, res, next) {
 };
 
 exports.saveEntry = function(req, res, next) {    
-    console.log(req.body["expiration"])
+
     let interval = "1 YEAR";
     if (req.body["expiration"] === "month"){
         interval = "1 MONTH";
@@ -35,7 +37,12 @@ exports.saveEntry = function(req, res, next) {
         interval = "5 MINUTES"
     }
 
-    db.query("INSERT INTO paste(text,secret,expiration,created_at) values(? , ? , DATE_ADD(NOW(), INTERVAL "+interval+"),NOW());",[req.body["text"],req.body["secret"]],function(err,result){
+    let secret = ""
+    if (req.body["secret"]){
+        secret = crypto.createHash('md5').update(req.body["secret"]).digest("hex");
+        console.log(secret)
+    }
+    db.query("INSERT INTO paste(text,secret,expiration,created_at) values(? , ? , DATE_ADD(NOW(), INTERVAL "+interval+"),NOW());",[req.body["text"],secret],function(err,result){
         if (err) throw err; 
         res.send( { "url": req.headers.host+"/p/"+utils.id2url(result.insertId+999999) } );
     });    
@@ -55,7 +62,7 @@ exports.decryptPaste = function(req, res,next ){
         if (err) return err;
         if (result[0] === undefined) return next();
         console.log(result[0].secret);
-        if (result[0].secret === req.body["secret"]){
+        if (result[0].secret === crypto.createHash('md5').update(req.body["secret"]).digest("hex")){
             res.send({ status: 200, content: result[0].text })            
         }else{
             res.send({ status: 400, content: "" })            
